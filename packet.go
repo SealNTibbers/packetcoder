58553398e65f7bbac6f07073ad81eb6d7feb26ce
micael <micaelalastor@gmail.com>
2018-09-13 12:03:01 +0300

    first meaningful commit. implementing basic functionality of bit scheme

--- packet.go
+++ packet.go
@@ -2,23 +2,63 @@ package packetcoder
 
 import (
 	. "bytes"
+	"errors"
 )
 
 type Packet struct {
 }
 
-func (p *Packet) EncodeTo(writeBuffer *Buffer) {
+type bitfield struct {
+	name   string
+	size   uint
+	offset uint
+}
+
+func (f *bitfield) bitSize() uint {
+	return f.size
+}
+
+type BitScheme struct {
+	fields map[string]*bitfield
+	size   uint
+}
+
+func NewBitScheme() *BitScheme {
+	scheme := new(BitScheme)
+	scheme.fields = make(map[string]*bitfield)
+	return scheme
+}
+
+func (s *Packet) EncodeTo(writeBuffer *Buffer) {
 
 }
 
-func (p *Packet) DecodeFrom(readBuffer *Buffer) {
+func (s *Packet) DecodeFrom(readBuffer *Buffer) {
 
 }
 
-func (p *Packet) SetBitField(fieldName string, fieldSize uint) {
+func (s *BitScheme) SetBitField(fieldName string, fieldSize uint) {
+	field := new(bitfield)
+	field.name = fieldName
+	field.size = fieldSize
+	field.offset = s.size
+	s.size += field.size
 
+	s.fields[fieldName] = field
 }
 
-func (p *Packet) SetStuffBits(fieldSize uint) {
+func (s *BitScheme) SetStuffBits(fieldName string, fieldSize uint) {
+	s.SetBitField(fieldName, fieldSize)
+}
 
+func (s *BitScheme) BitSize() uint {
+	return s.size
+}
+
+func (s *BitScheme) OffsetOf(fieldName string) (uint, error) {
+	field := s.fields[fieldName]
+	if field == nil {
+		return 0, errors.New("there is no such field as" + fieldName + "in this scheme")
+	}
+	return field.offset, nil
 }
--- packetcoder_test.go
+++ packetcoder_test.go
@@ -2,6 +2,7 @@ package packetcoder
 
 import (
 	"bytes"
+	"github.com/SealNTibbers/packetcoder/testutils"
 	"testing"
 )
 
@@ -22,9 +23,46 @@ func TestPacketDecode(t *testing.T) {
 	packet.DecodeFrom(buf)
 }
 
-func TestPacketSetFields(t *testing.T) {
-	packet := new(Packet)
-	packet.SetBitField("head", 4)
-	packet.SetBitField("type", 8)
-	packet.SetStuffBits(4)
+func TestSchemeSetFields(t *testing.T) {
+	scheme := NewBitScheme()
+	scheme.SetBitField("head", 4)
+	scheme.SetBitField("type", 8)
+	scheme.SetStuffBits("fill", 4)
+}
+
+func TestSchemeBitSize(t *testing.T) {
+	scheme := NewBitScheme()
+	scheme.SetBitField("head", 4)
+	scheme.SetBitField("type", 8)
+	scheme.SetStuffBits("fill", 4)
+
+	var size uint
+	size = scheme.BitSize()
+	testutils.ASSERT_UEQ(t, size, 16)
+
+	scheme.SetBitField("crc", 8)
+
+	size = scheme.BitSize()
+	testutils.ASSERT_UEQ(t, size, 24)
+
+	size = 0
+	for _, field := range scheme.fields {
+		size += field.bitSize()
+	}
+
+	testutils.ASSERT_UEQ(t, size, scheme.BitSize())
+}
+
+func TestSchemeFieldsOffset(t *testing.T) {
+	scheme := NewBitScheme()
+	scheme.SetBitField("head", 4)
+	scheme.SetBitField("type", 8)
+	scheme.SetStuffBits("fill", 4)
+	scheme.SetBitField("crc", 8)
+
+	offset, _ := scheme.OffsetOf("type")
+	testutils.ASSERT_UEQ(t, offset, 4)
+
+	offset, _ = scheme.OffsetOf("crc")
+	testutils.ASSERT_UEQ(t, offset, 16)
 }
